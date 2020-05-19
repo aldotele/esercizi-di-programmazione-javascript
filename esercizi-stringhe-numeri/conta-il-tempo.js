5652cd7277a3d83cd7cda601c4f00209528c1451
Aldo <aldo.telese@hotmail.it>
2020-05-19 10:13:34 +0200

    Update conta-il-tempo.js

--- esercizi-stringhe-numeri/conta-il-tempo.js
+++ esercizi-stringhe-numeri/conta-il-tempo.js
@@ -17,13 +17,27 @@ let TimeCount = (seconds) => {
   const hours = Math.floor(seconds/3600);
   const minutes = Math.floor([(seconds/3600) - hours] * 60);
   const secs = Math.floor(  [ [((seconds/3600) - hours) * 60] - minutes] * 60  )
-  return seconds + ' is ' + hours + ' hours' + ', ' + minutes + ' minutes ' + 'and ' + secs + ' seconds';
+
+  if (hours === 0) {
+    return seconds + ' is ' + minutes + ' minutes ' + 'and ' + secs + ' seconds';
+  } else if (minutes === 0 && secs === 0) {
+    return seconds + ' is ' + hours + ' hours';
+  } else if (minutes === 0) {
+    return seconds + ' is ' + hours + ' hours' + ' and ' + secs + ' seconds';
+  } else if (secs === 0) {
+    return seconds + ' is ' + hours + ' hours' + ' and ' + minutes + ' minutes';
+  }
+  return seconds + ' is ' + hours + ' hours' + ', ' + minutes + ' minutes ' + 'and ' + secs + ' seconds'
 }
 
 console.log(TimeCount(12560));
 console.log(TimeCount(13604));
 console.log(TimeCount(50060505));
 console.log(TimeCount(3600));
+console.log(TimeCount(7200));
+console.log(TimeCount(36000));
+
+
 
 
 
