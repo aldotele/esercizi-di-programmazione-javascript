f4f59270fc2efabb10b551a25f9c75f062c221b5
Aldo <aldo.telese@hotmail.it>
2020-05-20 11:33:16 +0200

    my solution to il-conta-cifre

--- esercizi-condizioni/il-conta-cifre.js
+++ esercizi-condizioni/il-conta-cifre.js
@@ -11,3 +11,24 @@
 
   http://www.imparareaprogrammare.it
 */
+
+
+const contaCifre = num => {
+  if(num >= 0 && num < 10) {
+    console.log('il numero ' + num + ' ha 1 cifra')
+  } else if(num >= 10 && num < 100) {
+    console.log('il numero ' + num + ' ha due cifre')
+  } else if(num >= 100 && num < 1000 ) {
+    console.log('il numero ' + num + ' ha 3 cifre')
+  } else if(num >= 1000 && num < 10000) {
+    console.log('il numero ' + num + ' ha 4 cifre')
+  } else {
+    console.log('numero non valido')
+  }
+}
+
+contaCifre('500')
+contaCifre(500)
+contaCifre(10000)
+contaCifre(9999)
+contaCifre('770')
