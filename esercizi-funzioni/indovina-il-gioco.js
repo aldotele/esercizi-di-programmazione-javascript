0b50d98a7565dbe39a723df9152ea70bf2b9ac20
Aldo <aldo.telese@hotmail.it>
2020-05-21 12:36:50 +0200

    my solution to indovina-il-gioco

--- esercizi-funzioni/indovina-il-gioco.js
+++ esercizi-funzioni/indovina-il-gioco.js
@@ -14,3 +14,42 @@
 
   http://www.imparareaprogrammare.it
 */
+
+const numVerify = num => {
+  let day = ''
+  if(num > 0 && num < 8) {
+      switch(num) {
+        case 1:
+        day = num + ' is Monday'
+        break;
+        case 2:
+        day = num + ' is Tuesday'
+        break;
+        case 3:
+        day = num + ' is Wednesday'
+        break;
+        case 4:
+        day = num + ' is Thursday'
+        break;
+        case 5:
+        day = num + ' is Friday'
+        break;
+        case 6:
+        day = num + ' is Saturday'
+        break;
+        case 7:
+        day = num + ' is Sunday'
+      }
+  } else {
+    console.log('cannot guess the day, sorry.')
+  }
+return console.log(day);
+}
+
+numVerify(7)
+numVerify(3)
+numVerify(1)
+numVerify(6)
+numVerify(8)
+numVerify(0)
+//capire perchè tra gli ultimi due function call viene lasciato uno spazio nella console
