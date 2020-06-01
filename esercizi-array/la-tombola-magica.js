7812e0f20812841428d3db78eee580307a412e73
Aldo <aldo.telese@hotmail.it>
2020-06-01 16:41:27 +0200

    my solution to la-tombola-magica

--- esercizi-array/la-tombola-magica.js
+++ esercizi-array/la-tombola-magica.js
@@ -19,3 +19,46 @@
 
   http://www.imparareaprogrammare.it
 */
+
+let myArray = [7, 8, 17, 22, 23, 32, 33, 45, 77, 99]
+let casualArray = []
+let n = 0
+
+for (i=0; i<10+n; i++) {
+   let randomNumber = Math.floor(Math.random()*(90-1)+1)
+   if (!casualArray.includes(randomNumber)) { // se il nuovo randomNumber generato non è già incluso nell'array, lo aggiungo
+     casualArray.push(randomNumber)
+   } else {
+     n+=1 //altrimenti se il randomNumber generato è già presente nell'array, aggiungo un'iterazione in più
+   } 
+}
+
+let mutualNumbers = []  // creo un'array che include i numeri in comune
+for(let j = 0; j < myArray.length; j++) {
+  for (let k = 0; k < casualArray.length; k++) { //comparo ogni elemento dell'array scelta con ogni elemento di quella casuale
+    if (myArray[j] === casualArray[k]) {
+      mutualNumbers.push(myArray[j]) // aggiungo ogni elemento comune nell'array mutualNumbers
+    }
+  }
+}
+
+console.log(myArray)
+console.log(casualArray)
+
+if(mutualNumbers.length===0 || mutualNumbers.length===1) {
+  console.log('mi dispiace hai perso')
+} else if(mutualNumbers.length===2) {
+  console.log('hai fatto AMBO')
+} else if(mutualNumbers.length===3) {
+  console.log('hai fatto TERNA')
+} else if(mutualNumbers.length===4) {
+  console.log('hai fatto QUATERNA')
+} else if(mutualNumbers.length===5) {
+  console.log('hai fatto CINQUINA')
+} else if(mutualNumbers.length===10) {
+  console.log('hai fatto TOMBOLA')
+}
+
+if(mutualNumbers.length>1) { // stampo i numeri in comune solo in caso di vittoria
+console.log('i numeri comuni alle due array: ' + mutualNumbers)  
+} 
