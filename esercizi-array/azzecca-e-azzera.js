45e517bb08ba71e4c5b37483b3ae4d959297cc9d
Aldo <aldo.telese@hotmail.it>
2020-06-09 10:43:07 +0200

    Update azzecca-e-azzera.js

--- esercizi-array/azzecca-e-azzera.js
+++ esercizi-array/azzecca-e-azzera.js
@@ -13,25 +13,47 @@
   http://www.imparareaprogrammare.it
 */
 
-// creo l'array da 100 elementi con numeri tra 1 e 50. Capire come fissarla
+// inizio a creare l'array di 100 elementi
 let array = []
-for (let i=0; i<100; i++) { 
-  array.push(Math.round(Math.random()*(50-1)+1))
+for (let i=0; i<100; i++) {
+  array[i] = Math.round(Math.random()*(99)+1)
 }
-console.log(array)
+//console.log(array)
 
-/* TESTANDO seconda parte esercizio 
+let notZero = true // mi serve nel while per fare in modo che continui a chiedere input di numeri fino a quando la condizione è true
+let input
 
-let arrTest = [5,7,12,4,7,9,43,22,56, 88]
+while (notZero) {
+  //input = Number(prompt("inserisci un numero"))
+  array = array.map(x => x % input ? x : 0)  // è necessario il termine a sinistra dell'espressione per poter far comparire l'array con gli zeri
+  //console.log(array)
+  for (i=0; i<array.length; i++) {
+    if (array[i]) { // è sufficiente scrivere la condizione in questo modo, perchè è TRUE se il numero è diverso da zero
+      notZero = true; // in altre parole, se tra tutte le array[i] ho un numero diverso da zero, continuo a chiedere input perchè notZero=true
+      break;
+    } else { // se tra tutte le array[i] non ne trovo neanche una che sia un numero diverso da zero, o in altre parole array[i]=false, allora notZero=false e il ciclo si ferma.
+      notZero = false
+    }
+  }
+}
+console.log('array azzerata. Ricaricare pagina.')
 
-let  number = window.prompt()      
+
+
+    /* TENTATIVO ERRATO INFINITY LOOP
+    let arrTest = [5,7,12,4,7,9,43,22,56, 88]
+
+
 for (let j=0; j<arrTest.length; j++) {
-  
-  if(arrTest[j]%number === 0) {
-    arrTest[j]=0
+  if (arrTest[j] !== 0) {
+    let number = prompt() 
+    for (let k=0; k<arrTest.length; k++) {  
+      if(arrTest[k]%number === 0) {
+      arrTest[k]=0
  
-  }   
-  
+      }   
+    }
+  } 
 }
 console.log(arrTest)
-    */
+*/
