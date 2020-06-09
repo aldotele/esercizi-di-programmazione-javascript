05c844333ac00f7ee6eea5f77495dc750480d32e
Aldo <aldo.telese@hotmail.it>
2020-06-09 11:52:40 +0200

    my solution to righe-o-colonne

--- esercizi-array-multidimensionali/righe-o-colonne.js
+++ esercizi-array-multidimensionali/righe-o-colonne.js
@@ -21,3 +21,44 @@
 
   http://www.imparareaprogrammare.it
 */
+
+// creo matrice NxM con numeri casuali tra 1 e 100
+let N = 2
+let M = 3
+let matrice = []
+let casuale
+
+for (let i=0; i<N; i++) {
+  matrice[i] = []
+  for (let j=0; j<M; j++) {  
+    casuale = Math.round(Math.random()*(99)+1)
+    matrice[i][j] = casuale
+  }
+}
+console.log(matrice)
+
+// creo array R in cui ogni elemento equivale alla somma delle righe di matrice
+let arrayR = []
+let sommaR=0 //al termine di ogni ciclo delle j azzero somma R perchè sto andando a sommare per i+1
+for (i=0; i<matrice.length; i++) {   // matrice.length equivale a N
+  sommaR = 0
+  for (j=0; j<matrice[i].length; j++) {  // matrice[i].length equivale a M
+    sommaR += matrice[i][j]   
+  }
+  arrayR[i]=sommaR
+}
+console.log(arrayR) 
+
+// creo array C
+let arrayC = []
+let sommaC = 0
+for (i=0; i<M; i++) {
+  sommaC=0
+  for (j=0; j<N; j++) {
+    sommaC += matrice[j][i]
+  }
+  arrayC[i]=sommaC
+     
+
+}
+console.log(arrayC) 
