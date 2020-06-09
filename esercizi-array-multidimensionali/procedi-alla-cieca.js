9292ca2e5a4b1ae036cb9afed595f8426c9e22e7
Aldo <aldo.telese@hotmail.it>
2020-06-09 10:54:10 +0200

    Update procedi-alla-cieca.js

--- esercizi-array-multidimensionali/procedi-alla-cieca.js
+++ esercizi-array-multidimensionali/procedi-alla-cieca.js
@@ -8,12 +8,25 @@
   http://www.imparareaprogrammare.it
 */
 
-//PROVA
-/*
+// scritto non come array ma andando accapo
+
 let risultato = ''
 for (i=0; i<20; i++) {
   for (j=0; j<20; j++) {
     risultato += ' ' + Math.floor(Math.random()*100)
+    
+    
+  }
+ risultato += '\n'
+}
+console.log(risultato)
+
+/* ESEMPIO matrice 15x20 con valori codice binario
+
+let risultato = ''
+for (i=0; i<15; i++) {
+  for (j=0; j<20; j++) {
+    risultato += ' ' + Math.round(Math.random()*1)
     
     
   }
