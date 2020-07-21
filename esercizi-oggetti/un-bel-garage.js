4cacd486e8fb09fbd3e381027e0384237a31fcf2
Aldo <aldo.telese@hotmail.it>
2020-07-21 19:12:35 +0200

    my-solution-to-un-bel-garage

--- esercizi-oggetti/un-bel-garage.js
+++ esercizi-oggetti/un-bel-garage.js
@@ -10,6 +10,38 @@
   http://www.imparareaprogrammare.it
 */
 
+function Car(brand, model, color, fuel) {
+  this.brand = brand;
+  this.model = model;
+  this.color = color;
+  this.fuel = fuel;
+}
+
+Car.prototype.numWheels = 4
+
+let OpelMokka = new Car('Opel', 'Mokka', 'Blue', 'diesel');
+let A6Dark = new Car('Audi', 'A6', 'anthracite', 'diesel');
+let A6Light = new Car('Audi', 'A6', 'grey', 'diesel');
+let LanciaY = new Car('Lancia', 'Ypsilon', 'white', 'gpl');
+let GrandePunto = new Car('Fiat', 'Punto', 'gunmetal grey', 'gasoline');
+
+let garage = [OpelMokka, A6Dark, A6Light, LanciaY, GrandePunto]
+
+let getCarByBrand = function(arg) {
+  let brandedCars = []
+  garage.forEach(function(el) {
+    if (el.brand.toLowerCase() === arg.toLowerCase()) {
+      brandedCars.push(el)
+    }    
+  })
+  return brandedCars.length > 0 ? brandedCars : 'no car by this brand';
+}
+
+console.log(getCarByBrand('Audi'));
+console.log(getCarByBrand('lancia'));
+console.log(getCarByBrand('OPeL'))
+console.log(getCarByBrand('FIAT'));
+console.log(getCarByBrand('Mercedes'));
 
 
 
